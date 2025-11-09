e250799c7dbcb0e327a68c8b4b35b2741a8ef678
Donaljojo <156642169+Donaljojo@users.noreply.github.com>
2025-11-09 18:34:22 +0000

    updates on analytics page

--- intrunex/src/Modules/Analytics/Controller/AnalyticsController.php
+++ intrunex/src/Modules/Analytics/Controller/AnalyticsController.php
@@ -2,6 +2,8 @@
 
 namespace App\Modules\Analytics\Controller;
 
+use App\Modules\AssetDiscovery\Repository\AssetRepository;
+use App\Modules\AssetVulnerability\Repository\VulnerabilityRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,8 +13,24 @@ class AnalyticsController extends AbstractController
 {
     #[Route('/analytics', name: 'analytics')]
     #[IsGranted('ROLE_USER')]
-    public function index(): Response
+    public function index(AssetRepository $assetRepository, VulnerabilityRepository $vulnerabilityRepository): Response
     {
-        return $this->render('analytics/index.html.twig');
+        $assetCount = $assetRepository->count([]);
+        $activeAssetsCount = $assetRepository->count(['status' => 'Active']);
+        $inactiveAssetsCount = $assetRepository->count(['status' => 'Inactive']);
+        $monitoredAssetsCount = $assetRepository->count(['isMonitored' => true]);
+        $unmonitoredAssetsCount = $assetRepository->count(['isMonitored' => false]);
+        $vulnerableAssetsCount = $vulnerabilityRepository->countVulnerableAssets();
+        $safeAssetsCount = $assetCount - $vulnerableAssetsCount;
+
+        return $this->render('analytics/index.html.twig', [
+            'assetCount' => $assetCount,
+            'activeAssetsCount' => $activeAssetsCount,
+            'inactiveAssetsCount' => $inactiveAssetsCount,
+            'monitoredAssetsCount' => $monitoredAssetsCount,
+            'unmonitoredAssetsCount' => $unmonitoredAssetsCount,
+            'vulnerableAssetsCount' => $vulnerableAssetsCount,
+            'safeAssetsCount' => $safeAssetsCount,
+        ]);
     }
 }
